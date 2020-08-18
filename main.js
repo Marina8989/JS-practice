f18ce1cc4adac50efae8812a5de088d6acbbf23b
Marina8989 <marina.mill209@gmail.com>
2020-08-17 23:30:39 -0700

    js-practice

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const span = document.querySelector('.color');
 
 const colors = ['green', 'red', 'rgb(135, 26, 78, 92)', '#ff4586'];
 
-btn.addEventListener('click', function() {
-  let randColor = Math.floor(Math.random() * colors.length);
-  
-  document.body.style.backgroundColor = colors[randColor];
-  span.textContent = colors[randColor];
+btn.addEventListener('click', function () {
+    let randColor = Math.floor(Math.random() * colors.length);
+
+    document.body.style.backgroundColor = colors[randColor];
+    span.textContent = colors[randColor];
 })
